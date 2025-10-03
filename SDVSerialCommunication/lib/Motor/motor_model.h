ad616b2ad9700a617f934d5a58e3389244bcf3c8
antorresca <antorresca@unal.edu.co>
2025-10-03 12:32:59 -0500

    Remove folder of Firmware

--- SDVSerialCommunication/lib/Motor/motor_model.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-enum MotorModel
-{
-    NONE = 0,
-    ESCON = 1,
-    POLOLU = 2
-};
--- SDVSerialCommunication/lib/Motor/tools.h
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-
-double getAbs(double val)
-{
-    if(val > 0.0)
-        return val;
-    else
-        return -val;
-}
