de1e878617bf3ba599e68720309cf52c95cd6179
Andres Torres <antorresca@unal.edu.co>
2025-10-28 21:30:31 -0500

    Eliminacion de algunos archivos (probar funcionamiento)

--- src/Sick_scan_xd/demo/run_mrs1xxx.sh
+++
@@ -1,3 +0,0 @@
-echo "Running MRS1xxx scanner - modify launch file parameter for special settings."
-../sick_generic_caller ./launch/sick_mrs_1xxx.launch
-
--- src/Sick_scan_xd/test/scripts/wireshark.cmd
+++
@@ -1,3 +0,0 @@
-..\..\tools\wireshark_win64_3.6.0_portable\WiresharkPortable64.exe
-@timeout /t 1
-rem @pause
