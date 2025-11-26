bd150ca8b2668c0223c1db6371aa5a84c788e60c
Andres Torres <antorresca@unal.edu.co>
2025-11-26 15:05:13 -0500

    actualizacion setup

--- src/sdv_nav/setup.py
+++ src/sdv_nav/setup.py
@@ -1,29 +1,39 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'sdv_nav'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.0.1',
+    packages=[package_name],
     data_files=[
+        # Install package.xml
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        # Install launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+
+        # Install urdf files (opcional pero recomendado)
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
+
+        # Install maps (si quieres dejarlo limpio)
+        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='jk',
-    maintainer_email='juagomezro@unal.edu.co',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer='andres',
+    maintainer_email='andres@example.com',
+    description='Bringup del robot SDV: TF, Lidar, AMCL, Map server',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            # Si no tienes nodos python deja esto vacío
         ],
     },
 )
