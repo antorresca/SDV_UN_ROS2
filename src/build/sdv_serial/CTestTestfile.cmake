72488ab4674bf2ece0904f7336f3f416f0fa9a95
Juan Camilo <juagomezro@unal.edu.co>
2025-11-26 14:55:37 -0500

    mapa

--- src/build/sdv_serial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/antorresca/Desktop/CursoDirigido/SDV_UN_ROS2/src/sdv_serial
-# Build directory: /home/antorresca/Desktop/CursoDirigido/SDV_UN_ROS2/src/build/sdv_serial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
