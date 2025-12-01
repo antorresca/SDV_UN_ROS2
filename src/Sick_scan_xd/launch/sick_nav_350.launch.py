4cf66852853b12795bff3912061254356cf3878e
antorresca <antorresca@unal.edu.co>
2025-12-01 15:29:31 -0500

    a

--- src/Sick_scan_xd/launch/sick_nav_350.launch.py
+++ src/Sick_scan_xd/launch/sick_nav_350.launch.py
@@ -9,56 +9,50 @@ def generate_launch_description():
 
     ld = LaunchDescription()
     sick_scan_pkg_prefix = get_package_share_directory('sick_scan_xd')
-    launchfile = os.path.basename(__file__)[:-3] # convert "<lidar_name>.launch.py" to "<lidar_name>.launch"
-    launch_file_path = os.path.join(sick_scan_pkg_prefix, 'launch/' + launchfile) # 'launch/sick_nav_350.launch')
+    
+    # Obtiene el nombre del archivo launch para pasarlo como argumento al caller
+    # Esto se resuelve a algo como 'sick_nav_350.launch'
+    launchfile = os.path.basename(__file__)[:-3] 
+    launch_file_path = os.path.join(sick_scan_pkg_prefix, 'launch/' + launchfile) 
     node_arguments=[launch_file_path]
     
-    # append optional commandline arguments in name:=value syntax
+    # Permite añadir argumentos adicionales desde la línea de comandos
     for arg in sys.argv:
         if len(arg.split(":=")) == 2:
             node_arguments.append(arg)
     
-    ROS_DISTRO = os.environ.get('ROS_DISTRO') # i.e. 'eloquent', 'foxy', etc.
-    if ROS_DISTRO[0] <= "e": # ROS versions eloquent and earlier require "node_executable", ROS foxy and later use "executable"
-        node = Node(
-            package='sick_scan_xd',
-            node_executable='sick_generic_caller',
-            output='log',
-            remappings=[ ('/sick_nav_350/scan', '/scan'), ],
-            arguments=node_arguments,
-                        parameters=[{
-                "use_odom": False,
-                "publish_odom_tf": False,
-                "publish_odom": False,
-                "output_odom_topic": "",
-            }]
-        )
-    else: 
-        node = Node(
-            package='sick_scan_xd',
-            executable='sick_generic_caller',
-            output='log',
-            remappings=[ ('/sick_nav_350/scan', '/scan'), ],
-            arguments=node_arguments,
-            parameters=[{
-                "use_odom": False,
-                "publish_odom_tf": False,
-                "publish_odom": False,
-                "output_odom_topic": "",
-                
-                # Deshabilitar si publica una TF de cloud (a veces es cloud_transform)
-                "cloud_transform": False, # <--- CAMBIO CRÍTICO (Desactivar si es posible)
+    # --- Parámetros específicos para ROS 2 Humble (Foxy y posteriores) ---
+    node = Node(
+        package='sick_scan_xd',
+        # 'executable' es lo correcto para ROS Foxy/Humble y posteriores
+        executable='sick_generic_caller',
+        output='log',
+        # Remapeo CRÍTICO: remapea el tópico por defecto del SICK al estándar /scan
+        remappings=[ ('/sick_nav_350/scan', '/scan'), ],
+        arguments=node_arguments,
+        parameters=[{
+            # --- Parámetros de Odometría (Deshabilitados) ---
+            "use_odom": False,
+            "publish_odom_tf": False,
+            "publish_odom": False,
+            "output_odom_topic": "",
+            
+            # --- Parámetros Críticos de TF para Integración ---
+            
+            # CRÍTICO: Deshabilita la publicación de TF del sensor.
+            # Esto evita el conflicto donde el driver publica 'map -> cloud'
+            "cloud_transform": False, 
 
-                # Forzar la coherencia: el scanner_frame será el frame_id real
-                "scanner_frame": "cloud",  # <--- CORREGIDO: Usar 'cloud'
-                
-                # A veces, este es el parámetro que realmente controla el frame_id en el header:
-                "frame_id": "cloud", # <--- Añadir este por seguridad
-                
-                "publish_laserscan": True,
-                "use_published_timestamp": False
-            }]
-        )
+            # Establece el frame_id que se usará en el header del mensaje /scan.
+            # Tu static_transform_publisher lo usará como frame hijo.
+            "scanner_frame": "cloud",
+            "frame_id": "cloud", 
+            
+            # --- Otros Parámetros ---
+            "publish_laserscan": True,
+            "use_published_timestamp": False
+        }]
+    )
     
     ld.add_action(node)
     return ld
